c64163d872f242511ed152c7e1de41a23d408f93
why-ashjoshi <yash.joshi0301@mail.com>
2025-04-15 19:19:38 +0530

    Initial commit

---
+++ src/services/firebase.js
@@ -0,0 +1,73 @@
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    orderBy,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+
+        apiKey: "AIzaSyCiOXbUpRmZ0hqgN0oBzj-mmtokADE3Vf4",
+        authDomain: "chat-room-eef72.firebaseapp.com",
+        projectId: "chat-room-eef72",
+        storageBucket: "chat-room-eef72.appspot.com",
+        messagingSenderId: "713001024237",
+        appId: "1:713001024237:web:43f42c80a0324e54ea2ae4"
+    
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loginWithGoogle() {
+    try {
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+
+        const { user } = await signInWithPopup(auth, provider);
+
+        return { uid: user.uid, displayName: user.displayName };
+    } catch (error) {
+        if (error.code !== 'auth/cancelled-popup-request') {
+            console.error(error);
+        }
+        return null;
+    }
+}
+
+async function sendMessage(roomId, user, text) {
+    try {
+        await addDoc(collection(db, 'chat-rooms', roomId, 'messages'), {
+            uid: user.uid,
+            displayName: user.displayName,
+            text: text.trim(),
+            timestamp: serverTimestamp(),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getMessages(roomId, callback) {
+    return onSnapshot(
+        query(
+            collection(db, 'chat-rooms', roomId, 'messages'),
+            orderBy('timestamp', 'asc')
+        ),
+        (querySnapshot) => {
+            const messages = querySnapshot.docs.map((x) => ({
+                id: x.id,
+                ...x.data(),
+            }));
+
+            callback(messages);
+        }
+    );
+}
+
+export { loginWithGoogle, sendMessage, getMessages };
